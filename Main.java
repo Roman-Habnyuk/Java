f9b8ede520317c7ffeef8af0fc3c56f551c0621c
Roman-Habnyuk <148562903+Roman-Habnyuk@users.noreply.github.com>
2024-05-29 14:55:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         // Створюємо студентів
-        Student student1 = new Student("Ivan", "Ivanov", "2000-01-01", 85);
-        Student student2 = new Student("Petr", "Petrov", "2001-02-02", 90);
-        Student student3 = new Student("Sergey", "Sidorov", "2002-03-03", 75);
-        Student student4 = new Student("Ivan", "Petrov", "2000-04-04", 80);
-        Student student5 = new Student("Anna", "Ivanova", "1999-12-12", 88);
-        Student student6 = new Student("Elena", "Alexeeva", "2001-11-11", 92);
-        Student student7 = new Student("Dmitry", "Dmitriev", "2002-10-10", 78);
-        Student student8 = new Student("Olga", "Kuznetsova", "2000-09-09", 81);
-        Student student9 = new Student("Alexey", "Smirnov", "2001-08-08", 89);
-        Student student10 = new Student("Maria", "Popova", "2002-07-07", 85);
+        Student student1 = new Student("Ivan", "Ivanov", LocalDate.parse("2000-01-01"), 85);
+        Student student2 = new Student("Petr", "Petrov", LocalDate.parse("2001-02-02"), 90);
+        Student student3 = new Student("Sergey", "Sidorov", LocalDate.parse("2002-03-03"), 75);
+        Student student4 = new Student("Ivan", "Petrov", LocalDate.parse("2000-04-04"), 80);
+        Student student5 = new Student("Anna", "Ivanova", LocalDate.parse("1999-12-12"), 88);
+        Student student6 = new Student("Elena", "Alexeeva", LocalDate.parse("2001-11-11"), 92);
+        Student student7 = new Student("Dmitry", "Dmitriev", LocalDate.parse("2002-10-10"), 78);
+        Student student8 = new Student("Olga", "Kuznetsova", LocalDate.parse("2000-09-09"), 81);
+        Student student9 = new Student("Alexey", "Smirnov", LocalDate.parse("2001-08-08"), 89);
+        Student student10 = new Student("Maria", "Popova", LocalDate.parse("2002-07-07"), 85);
 
         // Створюємо групу
         Group group = new Group("Group A");
